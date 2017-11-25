c4cf1e9940221294dbfb9293a24294e914a60293
HadiSDev <hads@itu.dk>
2017-11-25 02:06:48 +0100

    test

--- src/View/Client.java
+++ src/View/Client.java
@@ -17,18 +17,32 @@ import static SQL.MySQL.connectDB;
 
 public class Client extends Application {
 
-    private static Dimension  screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+    private static Dimension  screenSize = Toolkit.getDefaultToolkit().getScreenSize(); //gets screen resolution data
+
+    private double offset_x; //Start x point on mouse click
+    private double offset_y; //Start y point on mouse click
 
 
     @Override
-
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("design.fxml"));
+
+        Scene scene = new Scene(root, (screenSize.getWidth()/2), screenSize.getHeight()/2);
+
+        //when mouse is pressed anywhere on the scene it stores the position
+        scene.setOnMousePressed(event -> {
+            offset_x = event.getSceneX();
+            offset_y = event.getSceneY();
+        });
+
+        scene.setOnMouseDragged(event -> {
+            primaryStage.setX(event.getScreenX() - offset_x);
+            primaryStage.setY(event.getScreenY() - offset_y);
+        });
+
         primaryStage.initStyle(StageStyle.UNDECORATED);
-        primaryStage.setScene(new Scene(root, (screenSize.getWidth()/2), screenSize.getHeight()/2));
-
+        primaryStage.setScene(scene);
         primaryStage.show();
-
     }
     public static void main(String[] args)
     {
