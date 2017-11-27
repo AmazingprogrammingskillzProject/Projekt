5d9e85497211795087d33a11a02b376c4e108c9f
hadisdev <hads@itu.dk>
2017-11-27 15:40:59 +0100

    ts

---
+++ src/View/SceneViewer.java
@@ -0,0 +1,33 @@
+package View;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+import javafx.scene.shape.Rectangle;
+
+import java.awt.*;
+
+public class SceneViewer extends Application
+{
+    public void start(Stage Stage) throws Exception
+    {
+/*        BorderPane root = new BorderPane();
+        Scene scene = new Scene(root, Client.screenSize.getWidth()/3, Client.screenSize.getHeight()/2);
+
+
+        Rectangle r = new Rectangle();
+        r.setX(50);
+        r.setY(50);
+        r.setWidth(200);
+        r.setHeight(100);
+        r.setArcWidth(20);
+        r.setArcHeight(20);
+
+        root.setCenter(r);
+        Stage.setTitle("Scene");
+        Stage.setScene(scene);
+        Stage.show();*/
+
+    }
+}
