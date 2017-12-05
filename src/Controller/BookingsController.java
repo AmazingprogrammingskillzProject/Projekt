e12b3c70d3196db8bb162fac744114b52b7d6d70
HadiSDev <hads@itu.dk>
2017-12-05 21:26:39 +0100

    Merge branch 'master' of https://github.com/AmazingprogrammingskillzProject/Projekt
    # Conflicts:
    #	src/Controller/NewReservationController.java
    #	src/Modules/V1_Database.java
    #	src/View/NewReservation.java

--- src/Controller/BookingsController.java
+++ src/Controller/BookingsController.java
@@ -1,4 +1,97 @@
 package Controller;
 
-public class BookingsController {
+import Modules.V1_Bookings;
+
+
+import Modules.V1_Database;
+import com.jfoenix.controls.*;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import static Modules.V1_Database.getBookings;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.paint.Color;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+
+public class BookingsController implements Initializable {
+
+    @FXML
+    private JFXTextField seachnumber;
+
+    @FXML
+    private TableView<V1_Bookings> tablevew;
+
+    @FXML
+    private JFXButton searchbutt;
+
+    private String phonetyped;
+
+    @FXML
+    private TableColumn<V1_Bookings, String> phone;
+
+    @FXML
+    private TableColumn<V1_Bookings, Integer> BID, showid, row, firstseat, lastseat;
+
+    @FXML
+    private JFXTextField seachnumber2;
+
+    @FXML
+    private JFXTextField bookingID;
+
+    @FXML
+    private JFXButton cancel;
+
+
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle)
+    {
+        searchbutt.setOnMouseClicked(event -> {
+            ArrayList<V1_Bookings> bookingsfound = new ArrayList<>();
+            for(V1_Bookings b: getBookings())
+            {
+
+                if (b.getPhone().equals(seachnumber.getText()))
+                {
+                    bookingsfound.add(b);
+
+                }
+            }
+            ObservableList<V1_Bookings> bookingsfoundO = FXCollections.observableArrayList(bookingsfound);
+
+
+            phone.setCellValueFactory(new PropertyValueFactory<>("Phone"));
+            BID.setCellValueFactory(new PropertyValueFactory<>("ID"));
+            showid.setCellValueFactory(new PropertyValueFactory<>("Showing_ID"));
+            row.setCellValueFactory(new PropertyValueFactory<>("Row"));
+            firstseat.setCellValueFactory(new PropertyValueFactory<>("FirstSeat"));
+            lastseat.setCellValueFactory(new PropertyValueFactory<>("LastSeat"));
+            tablevew.setItems(bookingsfoundO);
+        });
+        cancel.setRipplerFill(Color.RED);
+        if(!(seachnumber2.getText().equals("")&&bookingID.getText().equals("")))
+        {
+            cancel.setOnMouseClicked(event -> {
+                V1_Database.DeleteBooking(seachnumber2.getText(), Integer.parseInt(bookingID.getText()));
+
+            });
+        }
+
+
+
+
+
+
+
+
+    }
 }
+
